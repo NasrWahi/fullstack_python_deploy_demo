1f1c6a766fa693db9df289a89d62c3b487cfc30e
NasrWahi <nasr.wahi@gmail.com>
2026-08-26 23:42:27 +0200

    front & back

---
+++ backend/src/backend/data_processing.py
@@ -0,0 +1,5 @@
+from backend.constants import DATA_DIRECTORY
+import pandas as pd 
+
+df = pd.read_csv(DATA_DIRECTORY / "Pokemon.csv")
+df["Type 2"] = df["Type 2"].fillna("missing")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from fullstack-python-deploy-demo!")
+
+
+if __name__ == "__main__":
+    main()
